7df5089d76ba3078f298cd08896b1e62cbea3f6e
RektPunk <rektpunk@gmail.com>
2024-10-08 13:12:30 +0900

    update task , objective, mapper

--- autolgb/basic.py
+++ autolgb/basic.py
@@ -1,3 +1,5 @@
+from enum import IntEnum, StrEnum
+from pathlib import Path
 from typing import Literal
 
 import numpy as np
@@ -6,13 +8,16 @@ import scipy.sparse
 from lightgbm.basic import Sequence
 
 TrainDataType = (
-    np.ndarray
+    str
+    | Path
+    | np.ndarray
     | pd.DataFrame
     | scipy.sparse.spmatrix
     | Sequence
     | list[Sequence]
     | list[np.ndarray]
 )
+PredictDataType = str | Path | np.ndarray | pd.DataFrame | scipy.sparse.spmatrix
 LabelType = list[float] | list[int] | np.ndarray | pd.Series | pd.DataFrame
 WeightType = list[float] | list[int] | np.ndarray | pd.Series
 PositionType = np.ndarray | pd.Series
@@ -20,3 +25,22 @@ InitScoreType = list[float] | list[list[float]] | np.ndarray | pd.Series | pd.Da
 GroupType = list[float] | list[int] | np.ndarray | pd.Series
 CategoricalFeatureConfiguration = list[str] | list[int] | Literal["auto"]
 FeatureNameConfiguration = list[str] | Literal["auto"]
+
+
+class Task(IntEnum):
+    binary: int = 1
+    multiclass: int = 2
+    continuous: int = 3
+
+
+class Objective(StrEnum):
+    binary: str = "binary"
+    multiclass: str = "multiclass"
+    regression: str = "regression"
+
+
+TASK_OBJECTIVE_MAPPER: dict[Task, Objective] = {
+    Task.binary: Objective.binary,
+    Task.multiclass: Objective.multiclass,
+    Task.continuous: Objective.regression,
+}
