a827d1510ef574cfb1f02319d8e94326920990cf
RektPunk <rektpunk@gmail.com>
2024-10-10 10:58:59 +0900

    remove dataclass

--- autolgb/dataset.py
+++ autolgb/dataset.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Any
 
 import lightgbm as lgb
@@ -9,70 +8,63 @@ from sklearn.utils.multiclass import type_of_target
 from autolgb.basic import (
     CategoricalFeatureConfiguration,
     FeatureNameConfiguration,
-    GroupType,
     InitScoreType,
     LabelType,
-    PositionType,
     Task,
     TrainDataType,
     WeightType,
 )
 
 
-@dataclass
 class Dataset:
-    data: TrainDataType
-    label: LabelType | None = None
-    reference: "Dataset" | None = None
-    weight: WeightType | None = None
-    group: GroupType | None = None
-    init_score: InitScoreType | None = None
-    feature_name: FeatureNameConfiguration = "auto"
-    categorical_feature: CategoricalFeatureConfiguration = "auto"
-    params: dict[str, Any] | None = None
-    free_raw_data: bool = True
-    position: PositionType | None = None
+    def __init__(
+        self,
+        data: TrainDataType,
+        label: LabelType | None = None,
+        weight: WeightType | None = None,
+        init_score: InitScoreType | None = None,
+        feature_name: FeatureNameConfiguration = "auto",
+        categorical_feature: CategoricalFeatureConfiguration = "auto",
+        params: dict[str, Any] | None = None,
+        free_raw_data: bool = True,
+    ) -> None:
+        self.data = data
+        self.label = label
+        self.weight = weight
+        self.init_score = init_score
+        self.feature_name = feature_name
+        self.categorical_feature = categorical_feature
+        self.params = params
+        self.free_raw_data = free_raw_data
 
-    def __post_init__(self):
-        if self.reference is None:
-            _task_str = type_of_target(y=self.label)
+        if label is not None:
+            _task_str = type_of_target(y=label)
             _task = getattr(Task, _task_str)
             if _task is None:
                 raise ValueError("Unsupported task.")
+
             self.task: Task = _task
-        else:
-            self.task = self.reference.task
-
-        if self.label:
-            if isinstance(self.label, pd.DataFrame):
-                if self.label.shape[1] > 1:
+            if isinstance(label, pd.DataFrame):
+                if label.shape[1] > 1:
                     raise ValueError("Dimension of label must 1")
-                self.label = pd.Series(self.label)
+                self.label = pd.Series(label)
 
-            if self.reference is None:
-                self.is_encode_needed = self.task in {Task.binary, Task.multiclass}
-                if self.is_encode_needed:
-                    self.label_encoder = LabelEncoder()
-                    self.label = self.label_encoder.fit_transform(self.label)
-            else:
-                self.is_encode_needed = self.reference.is_encode_needed
-                self.label_encoder = self.reference.label_encoder
-                self.label = self.reference.label_encoder.transform(self.label)
+            self._is_encode_needed = self.task in {Task.binary, Task.multiclass}
+            if self.is_encode_needed:
+                self._label_encoder = LabelEncoder()
+                self.label = self._label_encoder.fit_transform(self.label)
 
     @property
     def dtrain(self) -> lgb.Dataset:
         return lgb.Dataset(
             data=self.data,
             label=self.label,
-            reference=self.reference.dtrain if self.reference else None,
             weight=self.weight,
-            group=self.group,
             init_score=self.init_score,
             feature_name=self.feature_name,
             categorical_feature=self.categorical_feature,
             params=self.params,
             free_raw_data=self.free_raw_data,
-            position=self.position,
         )
 
     @property
@@ -81,4 +73,10 @@ class Dataset:
 
     @property
     def is_encode_needed(self) -> bool:
-        return getattr(self, "is_encode_needed", False)
+        return getattr(self, "_is_encode_needed", False)
+
+    @property
+    def label_encoder(self) -> LabelEncoder:
+        if not self.is_encode_needed:
+            raise ValueError("No label encoder exists.")
+        return self._label_encoder
