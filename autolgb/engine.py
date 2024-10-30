81b44d2667fba88744345769e049addf47be5359
RektPunk <rektpunk@gmail.com>
2024-10-30 10:52:19 +0900

    change docstring engine

--- autolgb/engine.py
+++ autolgb/engine.py
@@ -14,7 +14,7 @@ from autolgb.dataset import Dataset
 
 
 def param_update(params: dict[str, Any], train_set: Dataset) -> dict[str, Any]:
-    """Set objective and num_class for multiclass in params"""
+    """Set objective and num_class for multiclass in params."""
     if "objective" in params:
         return params
     _params = deepcopy(params)
@@ -35,7 +35,21 @@ def param_update(params: dict[str, Any], train_set: Dataset) -> dict[str, Any]:
 
 
 class Engine:
+    """A wrapper for LightGBM training, prediction, cross-validation, and hyperparameter optimization.
+    It provides a streamlined interface for training LightGBM models, making predictions,
+    performing cross-validation, and optimizing hyperparameters using Optuna."""
+
     def __init__(self, params: dict[str, Any] = {}, num_boost_round: int = 100):
+        """Initializes the Engine object.
+
+        Parameters
+        ----------
+        params : dict
+            Parameters for training.
+            Values passed through ``params`` take precedence over those supplied via arguments.
+        num_boost_round : int, optional (default=100)
+            Number of boosting iterations.
+        """
         self.params = params
         self.num_boost_round = num_boost_round
 
@@ -46,6 +60,25 @@ class Engine:
         keep_training_booster: bool = False,
         callbacks: list[Callable] | None = None,
     ) -> None:
+        """Trains a LightGBM model on the given dataset.
+
+        Parameters
+        ----------
+        train_set : Dataset
+            Data to be trained on.
+        init_model : str, pathlib.Path, Booster or None, optional (default=None)
+            Filename of LightGBM model or Booster instance used for continue training.
+        keep_training_booster : bool, optional (default=False)
+            Whether the returned Booster will be used to keep training.
+            If False, the returned value will be converted into _InnerPredictor before returning.
+            This means you won't be able to use ``eval``, ``eval_train`` or ``eval_valid`` methods of the returned Booster.
+            When your model is very large and cause the memory error,
+            you can try to set this param to ``True`` to avoid the model conversion performed during the internal call of ``model_to_string``.
+            You can still use _InnerPredictor as ``init_model`` for future continue training.
+        callbacks : list of callable, or None, optional (default=None)
+            List of callback functions that are applied at each iteration.
+            See Callbacks in Python API for more information.
+        """
         self.is_encode_needed = train_set.is_encode_needed
         if self.is_encode_needed:
             self.label_encoder = train_set.label_encoder
@@ -72,6 +105,47 @@ class Engine:
         data_has_header: bool = False,
         validate_features: bool = False,
     ) -> np.ndarray | spmatrix | list[spmatrix]:
+        """Makes predictions on the provided dataset.
+
+        Parameters
+        ----------
+        data : Dataset
+            Data source for prediction.
+        start_iteration : int, optional (default=0)
+            Start index of the iteration to predict.
+            If <= 0, starts from the first iteration.
+        num_iteration : int or None, optional (default=None)
+            Total number of iterations used in the prediction.
+            If None, if the best iteration exists and start_iteration <= 0, the best iteration is used;
+            otherwise, all iterations from ``start_iteration`` are used (no limits).
+            If <= 0, all iterations from ``start_iteration`` are used (no limits).
+        raw_score : bool, optional (default=False)
+            Whether to predict raw scores.
+        pred_leaf : bool, optional (default=False)
+            Whether to predict leaf index.
+        pred_contrib : bool, optional (default=False)
+            Whether to predict feature contributions.
+            .. note::
+                If you want to get more explanations for your model's predictions using SHAP values,
+                like SHAP interaction values,
+                you can install the shap package (https://github.com/slundberg/shap).
+                Note that unlike the shap package, with ``pred_contrib`` we return a matrix with an extra
+                column, where the last column is the expected value.
+
+        data_has_header : bool, optional (default=False)
+            Whether the data has header.
+            Used only if data is str.
+        validate_features : bool, optional (default=False)
+            If True, ensure that the features used to predict match the ones used to train.
+            Used only if data is pandas DataFrame.
+
+        Returns
+        -------
+        result : numpy array, scipy.sparse.spmatrix or list of scipy.sparse.spmatrix
+            Prediction result.
+            Can be sparse or a list of sparse objects (each element represents predictions for one class) for feature contributions (when ``pred_contrib=True``).
+
+        """
         self.__check_fitted()
         _predict = self.booster.predict(
             data=data.dpredict,
@@ -88,21 +162,20 @@ class Engine:
             or pred_leaf
             or pred_contrib
             or isinstance(_predict, spmatrix | list)
+            or not self.is_encode_needed
         ):
             return _predict
 
-        if self.is_encode_needed:
-            if len(self.label_encoder.classes_) > 2:
-                class_index = np.argmax(_predict, axis=1)
-                return self.label_encoder.inverse_transform(class_index)
-            else:
-                class_index = np.round(_predict).astype(int)
-                return self.label_encoder.inverse_transform(class_index)
-        return _predict
+        class_index = (
+            np.argmax(_predict, axis=1)
+            if len(self.label_encoder.classes_) > 2
+            else np.round(_predict).astype(int)
+        )
+        return self.label_encoder.inverse_transform(class_index)
 
     def cv(
         self,
-        train_set: lgb.Dataset,
+        train_set: Dataset,
         folds: Iterable[tuple[np.ndarray, np.ndarray]]
         | BaseCrossValidator
         | None = None,
@@ -120,10 +193,59 @@ class Engine:
         eval_train_metric: bool = False,
         return_cvbooster: bool = False,
     ) -> dict[str, list[float] | lgb.CVBooster]:
+        """Perform cross-validation on the provided training set.
+
+        Parameters
+        ----------
+        train_set : Dataset
+            Data to be trained on.
+        folds : generator or iterator of (train_idx, test_idx) tuples, scikit-learn splitter object or None, optional (default=None)
+            If generator or iterator, it should yield the train and test indices for each fold.
+            If object, it should be one of the scikit-learn splitter classes
+            (https://scikit-learn.org/stable/modules/classes.html#splitter-classes)
+            and have ``split`` method.
+            This argument has highest priority over other data split arguments.
+        nfold : int, optional (default=5)
+            Number of folds in CV.
+        stratified : bool, optional (default=True)
+            Whether to perform stratified sampling.
+        shuffle : bool, optional (default=True)
+            Whether to shuffle before splitting data.
+        init_model : str, pathlib.Path, Booster or None, optional (default=None)
+            Filename of LightGBM model or Booster instance used for continue training.
+        fpreproc : callable or None, optional (default=None)
+            Preprocessing function that takes (dtrain, dtest, params)
+            and returns transformed versions of those.
+        seed : int, optional (default=0)
+            Seed used to generate the folds (passed to numpy.random.seed).
+        callbacks : list of callable, or None, optional (default=None)
+            List of callback functions that are applied at each iteration.
+            See Callbacks in Python API for more information.
+        eval_train_metric : bool, optional (default=False)
+            Whether to display the train metric in progress.
+            The score of the metric is calculated again after each training step, so there is some impact on performance.
+        return_cvbooster : bool, optional (default=False)
+            Whether to return Booster models trained on each fold through ``CVBooster``.
+
+        Returns
+        -------
+        eval_results : dict
+            History of evaluation results of each metric.
+            The dictionary has the following format:
+            {'valid metric1-mean': [values], 'valid metric1-stdv': [values],
+            'valid metric2-mean': [values], 'valid metric2-stdv': [values],
+            ...}.
+            If ``return_cvbooster=True``, also returns trained boosters wrapped in a ``CVBooster`` object via ``cvbooster`` key.
+            If ``eval_train_metric=True``, also returns the train metric history.
+            In this case, the dictionary has the following format:
+            {'train metric1-mean': [values], 'valid metric1-mean': [values],
+            'train metric2-mean': [values], 'valid metric2-mean': [values],
+            ...}.
+        """
         _params = param_update(params=self.params, train_set=train_set)
         return lgb.cv(
             params=_params,
-            train_set=train_set,
+            train_set=train_set.dtrain,
             num_boost_round=self.num_boost_round,
             folds=folds,
             nfold=nfold,
@@ -155,6 +277,37 @@ class Engine:
         seed: int = 0,
         callbacks: list[Callable] | None = None,
     ) -> None:
+        """Optimizes the model's hyperparameters using Optuna and cross-validation.
+
+        Parameters
+        ----------
+        train_set : Dataset
+            Data to be trained on.
+        ntrial : int, optional (default=10)
+            Number of optimize trials.
+        folds : generator or iterator of (train_idx, test_idx) tuples, scikit-learn splitter object or None, optional (default=None)
+            If generator or iterator, it should yield the train and test indices for each fold.
+            If object, it should be one of the scikit-learn splitter classes
+            (https://scikit-learn.org/stable/modules/classes.html#splitter-classes)
+            and have ``split`` method.
+            This argument has highest priority over other data split arguments.
+        nfold : int, optional (default=5)
+            Number of folds in CV.
+        stratified : bool, optional (default=True)
+            Whether to perform stratified sampling.
+        shuffle : bool, optional (default=True)
+            Whether to shuffle before splitting data.
+        init_model : str, pathlib.Path, Booster or None, optional (default=None)
+            Filename of LightGBM model or Booster instance used for continue training.
+        fpreproc : callable or None, optional (default=None)
+            Preprocessing function that takes (dtrain, dtest, params)
+            and returns transformed versions of those.
+        seed : int, optional (default=0)
+            Seed used to generate the folds (passed to numpy.random.seed).
+        callbacks : list of callable, or None, optional (default=None)
+            List of callback functions that are applied at each iteration.
+            See Callbacks in Python API for more information.
+        """
         _task_metric_mapper: dict[Task, str] = {
             Task.binary: "valid binary_logloss-mean",
             Task.multiclass: "valid multi_logloss-mean",
